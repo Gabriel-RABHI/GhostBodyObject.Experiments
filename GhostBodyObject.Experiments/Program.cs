c93d0015bb293032c5b3396c5bf126d8f6a4c63e
Gabriel RABHI <gabriel.rabhi@gmail.com>
2025-12-16 15:11:30 +0100

    Ajoutez des fichiers projet.

---
+++ GhostBodyObject.Experiments/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Threading.Tasks;
+using GhostBodyObject.BenchmarkRunner;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // One line start
+        await BenchmarkEngine.DiscoverAndShowAsync();
+    }
+}
+
+// -------------------------------------------------------------
+// SAMPLE BENCHMARK CLASS
+// -------------------------------------------------------------
+public class SampleBenchmarks : BenchmarkBase
+{
+    private const int COUNT = 1_000_000;
+    private const int THREAD_COUNT = 8;
+
+    [BruteForceBenchmark("S01", "Standard Sequential Loop", "CPU")]
+    public void SequentialTest()
+    {
+        RunMonitoredAction(() =>
+        {
+            double sum = 0;
+            for (int i = 0; i < COUNT; i++)
+            {
+                sum += Math.Sqrt(i);
+            }
+        })
+        .PrintToConsole($"Math.Sqrt {COUNT:N0} times")
+        .PrintDelayPerOp(COUNT)
+        .PrintSpace();
+    }
+
+    [BruteForceBenchmark("P01", "Parallel String Allocation", "Memory")]
+    public void ParallelTest()
+    {
+        RunParallelAction(THREAD_COUNT, (threadId) =>
+        {
+            // Tight loop inside user delegate
+            for (int i = 0; i < COUNT; i++)
+            {
+                string s = "test" + i;
+            }
+        })
+        .PrintToConsole($"Allocating {COUNT * THREAD_COUNT:N0} strings")
+        .PrintDelayPerOp(COUNT * THREAD_COUNT)
+        .PrintSpace();
+
+        
+
+        RunParallelAction(THREAD_COUNT, (threadId) =>
+        {
+            // Tight loop inside user delegate
+            for (int i = 0; i < COUNT; i++)
+            {
+                string s = "test" + i;
+            }
+            
+        })
+        .PrintToConsole($"Allocating {COUNT * THREAD_COUNT:N0} strings")
+        .PrintDelayPerOp(COUNT * THREAD_COUNT)
+        .PrintSpace();
+    }
+}
